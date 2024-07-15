7218b7df5e6d6f4e303e070840e6827b0bcbe7f9
AuBousquet <aurelie.bousquet@oslandia.com>
2024-07-15 19:01:33 +0200

    Rename variables and replace Config class by YAML dictionnary

--- tests/test_train.py
+++ tests/test_train.py
@@ -32,6 +32,7 @@ def test_get_test_save_path(fixture_path):
 
 def test_get_train_save_path():
     """Function get_train_save_path"""
+    """Function get_train_save_path"""
     assert trainer.get_train_save_path(output_dir=None, chosen_log=None) is None
     log_dir = Path(__file__).parent / "Log_test"
     train_path = trainer.get_train_save_path(output_dir=None, chosen_log=log_dir)
@@ -77,7 +78,10 @@ def test_train(dataset_path, trained_model_path):
     # First run
     train.train(
         dataset_path,
+        dataset_path,
         chosen_log=None,
+        output_dir=trained_model_path,
+        dataset=dataset_path.name,
         output_dir=trained_model_path,
         dataset=dataset_path.name,
         max_epoch=max_epoch,
@@ -110,6 +114,7 @@ def test_train(dataset_path, trained_model_path):
     )
 
     log_dirs = list(trained_model_path.iterdir())
+    log_dirs = list(trained_model_path.iterdir())
     assert len(log_dirs) == 1
     new_log_dir = log_dirs[0]
     assert new_log_dir == log_dir
@@ -122,37 +127,6 @@ def test_train(dataset_path, trained_model_path):
     assert (log_dir / "potentials" / "Area_5.ply").exists()
     assert (log_dir / f"val_preds_{max_epoch}" / "Area_5.ply").exists()
     assert (log_dir / "val_IoUs.txt").exists()
-
-
-@mark.dependency(depends=["test_train"])
-def test_test_validation_case(dataset_path, training_log):
-    """Functional test for inference process
-
-    First case: default inference on the validation dataset (Area_5.ply).
-
-    Should produce the following tree:
-
-    _ tests/fixtures/trained_models/Log_<date>/test/
-        |_ potentials/
-            |_ Area_5.ply
-        |_ probs/
-            |_ Area_5.ply
-        |_ predictions/
-            |_ Area_5.ply
-
-    """
-    test.test(
-        dataset_path,
-        None,
-        training_log,
-        dataset_path.name,
-        n_votes=1,
-        validation_size=5,
-        potential_increment=2,  # The min potential increases until ~2.5
-    )
-    assert (training_log / "test" / "potentials" / "Area_5.ply").is_file()
-    assert (training_log / "test" / "probs" / "Area_5.ply").is_file()
-    assert (training_log / "test" / "predictions" / "Area_5.ply").is_file()
 
 
 @mark.dependency(depends=["test_train"])
@@ -201,3 +175,34 @@ def test_test_inference_case(dataset_path, inference_file, training_log):
 
     # Clean out the inference folder
     rmtree(inference_file.parent / "test")
+
+
+@mark.dependency(depends=["test_train"])
+def test_test_validation_case(dataset_path, training_log):
+    """Functional test for inference process
+
+    First case: default inference on the validation dataset (Area_5.ply).
+
+    Should produce the following tree:
+
+    _ tests/fixtures/trained_models/Log_<date>/test/
+        |_ potentials/
+            |_ Area_5.ply
+        |_ probs/
+            |_ Area_5.ply
+        |_ predictions/
+            |_ Area_5.ply
+
+    """
+    test.test(
+        dataset_path,
+        None,
+        training_log,
+        dataset_path.name,
+        n_votes=1,
+        validation_size=5,
+        potential_increment=2,  # The min potential increases until ~2.5
+    )
+    assert (training_log / "test" / "potentials" / "Area_5.ply").is_file()
+    assert (training_log / "test" / "probs" / "Area_5.ply").is_file()
+    assert (training_log / "test" / "predictions" / "Area_5.ply").is_file()
