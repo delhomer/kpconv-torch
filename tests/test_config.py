f8d19d90f5abb9dcf0363e8ff430d08a51e08fb0
Aurélie Bousquet <aurelie.bousquet@oslandia.com>
2024-07-30 20:20:29 +0200

    Add unit tests for config.py functions

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,13 +1,26 @@
+import os
 import pytest
 
-from kpconv_torch.utils.config import valid_dataset
+from kpconv_torch.utils.config import valid_dataset, load_config, save_config
 
 
 def test_valid_dataset():
-    """If the dataset is supported, valid_dataset returns it, otherwise it raises an argparse
-    error.
-
+    """
+    If the dataset is supported, valid_dataset returns it, otherwise it raises a ValueError.
     """
     assert valid_dataset("S3DIS") == "S3DIS"
     with pytest.raises(ValueError):
         valid_dataset("wrong_dataset")
+
+
+def test_load_config():
+    assert load_config("tests/config_S3DIS.yml")["dataset"] == "S3DIS"
+
+
+def test_save_config():
+    config = load_config("tests/config_S3DIS.yml")
+    config["input"]["test_new_item"] = "new"
+
+    save_config("tests", config)
+    assert load_config("tests/config.yml")["input"]["test_new_item"] == "new"
+    os.remove("tests/config.yml")
